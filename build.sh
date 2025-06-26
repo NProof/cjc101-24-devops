427821ab136fe4f81bd82cbf868968606f75bc1c
NProof <endles00000@gmail.com>
2025-06-26 10:20:06 +0800

    ver4.0 first commit

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 set -ex
 #set registry
-USERNAME=129729052534.dkr.ecr.ap-northeast-1.amazonaws.com
+USERNAME=http://360121241118.dkr.ecr.ap-northeast-1.amazonaws.com/
 # image name
-IMAGE=iii-devops-ecr
+IMAGE=cjc101-24-ecr-repo
 docker build -t $USERNAME/$IMAGE:latest .
 version=`cat VERSION`
 echo "version: $version"
